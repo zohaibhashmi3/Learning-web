c9376dc485fea96dc1a66ebef86f04bae130edf0
zohaibhashmi3 <zohaibhashmi905@gmail.com>
2024-06-05 02:37:31 -0700

    java
    js table

---
+++ indus.js
@@ -0,0 +1,110 @@
+// console.log( "Hello world");
+// alert( " you cant enter");
+// document.write( "world");
+
+
+
+
+// // find the area  OF rectangle where the length is 5 and the width is 8
+// let length = 5 ;
+// let width =8;
+// let area=length*width;
+// con
+
+// 1. Input four numbers and display sum of these.
+let sum1=6;
+let sum2=3;
+let sum3=9;
+let sum4=7;
+let  totalsum = sum1+sum2+sum3+sum4;
+console.log (totalsum);
+
+// 2. Input marks of five subjects of a student and calculate its average
+
+let num1=20;
+let num2=30;
+let num3=40;
+let num4=50;
+let num5=60;
+let totalnum = (num1+num2+num3+num4+num5)/5;
+console.log ( totalnum);
+
+// 3. Input radius of circle and calculate area of circle.
+
+let radius=4;
+const PI = 3.14;
+let circle = PI * radius**2 ;
+console.log(circle);
+
+// 4. Take input from user about temperature in Celsius and display on screen the equivalent 
+//  Fahrenheit temperature
+
+let celsius=30;
+let Fahrenheit=(9/5 * celsius) + 32;
+console.log ( Fahrenheit)
+
+// 5. Calculate the pay of an employee, given the hours worked and rate per hour
+
+let hours=30;
+let rateperhour=100;
+let payoffemployee = 30*100; 
+console.log (payoffemployee)
+
+// 6. A painter wants to know the amount of paint needed to paint only the four walls. Each wall has 
+// same rectangular area. The chosen paint covers 100 square feet per gallon. 
+
+let width2 = 15;
+let hight2 =  20;
+let areaof2wall = width2 +hight2;
+ let areaof4wall = areaof2wall*4;
+let gallon = 100;
+let totalpaint=areaof4wall/100;;
+console.log( totalpaint);
+
+// 7. Bob would like to know what percentage of his income his rent is. Write a pseudo code which 
+// will calculate and print the required percentage
+
+let income = 6600;
+let rent =2000;
+let percentage = (rent/income) * 100;
+console.log (percentage);
+
+// 8. Write pseudo code to calculate circumference of circle
+ let diameter = 7.7;
+ const PIE = 3.14;
+ let circumference = PIE * diameter ;
+ console.log (circumference );
+
+//  10.Jim is going to replant his lawn. He would like to know how much the sod will cost, given cost of 
+//  the sod per square feet and the length and width of two different lawn areas in feet. Write a 
+//  solution to calculate this cost
+
+let lawn1 = 77*66;
+let lawn2 = 56*33;
+let sodcost = 4.7
+let totalcost = lawn1 + lawn2 ;
+console.log ( totalcost) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
